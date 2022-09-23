4918d2d4bd11b3182f6edc382bbfe4a8d397562d
borodinets <assassqwas@gmail.com>
2022-09-23 15:24:36 +0500

    2

---
+++ lesson001/home1.py
@@ -0,0 +1,15 @@
+a = int(input('введите число : '))
+if a < 1 or a > 7:
+
+    print("error");
+
+else:
+
+    if a < 6:
+    
+        print("нет");
+    
+    else:
+    
+       print("да");
+    

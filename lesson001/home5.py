4a5bed64ff72cfed87ce14add9b281428174e7dd
borodinets <assassqwas@gmail.com>
2022-09-25 14:21:18 +0500

    finish

---
+++ lesson001/home5.py
@@ -0,0 +1,15 @@
+# Напишите программу, которая по заданному номеру четверти,
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+querter = int(input('введите чеьверть: '))
+
+if querter == 1:
+    print('x > 0, y > 0')
+elif querter == 2:
+    print('x < 0, y > 0')
+elif querter == 3:
+    print('x < 0, y < 0')
+elif querter == 4:
+    print('x > 0, y < 0')
+else:
+    print('error')
